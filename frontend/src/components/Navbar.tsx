b708390b12c03cd96371ba48185e7f1b6755d776
Verónica Urgell del Prado <urgelv@rpi.edu>
2025-11-07 23:27:31 -0500

    Add Profile to Nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,9 +12,6 @@ function Navbar() {
     <>
     <div className="flex justify-between items-center p-4 text-input-foreground bg-header border-b border-b-border">
     <div className="flex items-center space-x-4">
-      <a href="/" className="text-l font-bold">YACS</a>
-      <ClassSearch/>
-    </div>
     <div className="flex items-center space-x-3 h-6 invisible sm:visible">
       <NavLink to="/planner" className={link}>4-Year Plan</NavLink>
       <Separator orientation="vertical" />
